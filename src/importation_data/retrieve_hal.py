28c8a43ef16598c981d1ffb716ad6a26158f0445
alan <alanroussel@free.fr>
2021-11-15 09:49:12 +0100

    qlq changements

--- src/importation_data/retrieve_hal.py
+++ src/importation_data/retrieve_hal.py
@@ -3,14 +3,15 @@ import pandas as pd
 
 
 
-def req_to_HAL_pd(requete):
-    """Faire la requête à HAL.
+def req_to_pd(requete):
+    """Fait la requête pour récuperer l'ensemble des publications du laboratoire
 
     :param str requete: requête url
     :return dataframe df: dataframe de la réponse
     """
     res = r.get(requete).json()['response']['docs']
     df = pd.json_normalize(res)
+    
     return df
 
 
@@ -20,6 +21,6 @@ def api_to_csv(fichier_hal="", query=""):
     :param fichier_hal: str du fichier à enregistrer
     """
     if fichier_hal:
-        df = req_to_HAL_pd(query)
+        df = req_to_pd(query)
         df.rename(columns={"doiId_s": "DOI", "halId_s": "Réf. HAL", "title_s": "Titre"}, inplace=True)
         df.to_csv("../data/dois/hal_from_api.csv", index=False, encoding='utf8', sep=';')
