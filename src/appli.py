6499eb578aa36f2dcb1295c097309b1ae3b5a951
alan <alanroussel@free.fr>
2021-11-18 18:50:27 +0100

    first graphique commit

--- src/appli.py
+++ src/appli.py
@@ -6,13 +6,14 @@ from importation_data.retrieve_hal import api_to_csv
 from fonctions_analyse.recuperer_data import enrich_to_csv
 from fonctions_analyse.ajouter_apc import ajout_apc
 from fonctions_analyse.aligner_data import aligner
+from visualisation.graphique import graphique_discipline
 # Lecture des paramètres du projet
 # import sys
 # print(sys.path)
 
 with open("../settings.json") as json_file:
     donnees = json.load(json_file)
-
+'''
 api_to_csv(fichier_hal=donnees["data"]["dois"]["hal_fichier_api"], query=donnees["query"])
 
 stats, df_charge = chargement_tout(
@@ -20,6 +21,9 @@ stats, df_charge = chargement_tout(
 
 df_charge = enrich_to_csv(df=df_charge, email=donnees["mail"], progression_denominateur=100)
 
-ajout_apc(df=df_charge, data_apc=donnees["data"]["apc_tracking"])
+# ajout_apc(df=df_charge, data_apc=donnees["data"]["apc_tracking"]) #une fois que je l'ai chargé, je le commente pour pas que ça le refasse
 
 aligner()
+'''
+
+graphique_discipline()
