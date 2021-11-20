79bba993462f2407b2146fc32a54b43c097556cc
alan <alanroussel@free.fr>
2021-11-20 11:05:23 +0100

    r

--- src/appli.py
+++ src/appli.py
@@ -13,17 +13,16 @@ from visualisation.graphique import graphique_discipline
 
 with open("../settings.json") as json_file:
     donnees = json.load(json_file)
-'''
+
 api_to_csv(fichier_hal=donnees["data"]["dois"]["hal_fichier_api"], query=donnees["query"])
 
 stats, df_charge = chargement_tout(
     donnees=donnees["data"]["dois"], api_hal=True, recherche_erreur=True)
-
+print('\nchargement fini\n')
 df_charge = enrich_to_csv(df=df_charge, email=donnees["mail"], progression_denominateur=100)
-
-# ajout_apc(df=df_charge, data_apc=donnees["data"]["apc_tracking"]) #une fois que je l'ai chargé, je le commente pour pas que ça le refasse
-'''
+print('\nenrichissement fini\n')
+ajout_apc(df=df_charge, data_apc=donnees["data"]["apc_tracking"]) #une fois que je l'ai chargé, je le commente pour pas que ça le refasse
+print('\najout apc fini\n')
 aligner()
 
 
-#graphique_discipline()
