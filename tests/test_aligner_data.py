132d503559772abc00c46f1908881f60f9bc0712
alan <alanroussel@free.fr>
2022-01-03 21:16:16 +0100

    test_aligner_data correction bug

--- tests/test_aligner_data.py
+++ tests/test_aligner_data.py
@@ -12,7 +12,7 @@ with open("../settings.json") as json_file:
 referentials = donnees["data"]["match_ref"]
 choixDomaines = donnees["choixDomaines"]
 
-with open("../data/match_referentials.json") as json_file:
+with open("./data/match_referentials.json") as json_file:
     match_referencial_expected = j.load(json_file)
 
 df_charge = pd.read_csv(
@@ -22,7 +22,7 @@ class TestAlignerData(unittest.TestCase):
 
     def test_importation(self):
         
-        with open("../data/"+referentials) as json_file:
+        with open("./data/"+referentials) as json_file:
             match_ref = j.load(json_file)
         self.assertEqual(match_ref,match_referencial_expected)
         self.assertIsInstance(df_charge, pd.DataFrame)
