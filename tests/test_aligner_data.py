dd1b1715bc26622489a75277a6620c8cc51198cd
alan <alanroussel@free.fr>
2022-01-03 14:09:14 +0100

    test aligner data : importation et fonction

--- tests/test_aligner_data.py
+++ tests/test_aligner_data.py
@@ -1,33 +1,44 @@
 import unittest
 import sys
 import pandas as pd
-import json
 import pandas.testing as pd_testing
 import json as j
-sys.path.append("../") # ? 
+sys.path.append("../")  # ?
+from src.fonctions_analyse.aligner_data import aligner
+
 
 with open("../settings.json") as json_file:
-    donnees = json.load(json_file)
+    donnees = j.load(json_file)
 referentials = donnees["data"]["match_ref"]
 choixDomaines = donnees["choixDomaines"]
 
 with open("../data/match_referentials.json") as json_file:
-    match_referencial_expected = json.load(json_file)
+    match_referencial_expected = j.load(json_file)
 
 df_charge = pd.read_csv(
     "../resultats/fichiers_csv/df_metadonnees.csv", encoding="utf8")
-
 class TestAlignerData(unittest.TestCase):
         # charger et exécuter. Vérifier les types
 
-    def test_match_referentials(self):
+    def test_importation(self):
+        
         with open("../data/"+referentials) as json_file:
-            match_ref = json.load(json_file)
+            match_ref = j.load(json_file)
         self.assertEqual(match_ref,match_referencial_expected)
+        self.assertIsInstance(df_charge, pd.DataFrame)
+        self.assertIsInstance(referentials, str)
+        self.assertIsInstance(choixDomaines, dict)
+        self.assertFalse(df_charge.empty)
 
     def test_aligner_data(self):
-        self.assertIsInstance(df_charge, pd.DataFrame)
-        self.assertFalse(df_charge.empty)
+        df = aligner(df=df_charge, referentials=donnees["data"]["match_ref"],
+                     choixDomaines=donnees["choixDomaines"])
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertFalse(df.empty)
+        self.assertTrue(type(df.is_oa[0]), bool)
+    
+    
+        
         
 if __name__ == '__main__':
     unittest.main()
